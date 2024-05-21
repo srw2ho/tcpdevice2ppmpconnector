d2a0b9ef4b9ba72e88e6cc7a61969f348593a99e
balciselman <selman.balci@gmail.com>
2024-05-21 16:43:45 +0200

    update docker building

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,36 @@
-FROM python:3.9-slim-buster
+# FROM python:3.11-slim-buster
+ARG PYTHON_VERSION="3.11-bookworm"
+
+FROM python:$PYTHON_VERSION
+
+# FROM python:3.9-slim-buster
 # FROM python:3.8-slim-buster
 # FROM python:3.8.0-slim
+
+
 
 RUN apt update && apt install -y git gcc
 RUN git config --global credential.helper cache
 
 COPY ./ ./
 
+# Enable Virtual Environment
+ENV VIRTUAL_ENV=/opt/venv
+RUN python -m venv $VIRTUAL_ENV
+RUN . $VIRTUAL_ENV/bin/activate
+
+RUN pip install --upgrade pip
+
 RUN pip install Cython
+
+
 
 RUN pip install git+https://github.com/srw2ho/tcpdevice2ppmpconnector.git
 
+
+RUN pip install  pip-licenses
+RUN pip-licenses --with-system --with-urls --order=license > ./python_dependencies.txt
+
+RUN pip uninstall -y Cython pip-licenses 
 
 ENTRYPOINT tail -f /dev/null
--- builddocker.sh
+++ builddocker.sh
@@ -8,7 +8,11 @@ echo -e "lese aus Datei dockerversion.txt die Version und schreibe diese into Bu
 
 Build=$(<dockerversion.txt)
 
-
+# PYTHON_INSTALLVERSION=3.11-bookworm # for RASPI 5
+PYTHON_INSTALLVERSION=3.11-slim-buster # for RASPI 5
+# PYTHON_INSTALLVERSION=3.9-slim-bookworm # for RASPI 4
+# PYTHON_INSTALLVERSION=3.9-bookworm # for RASPI 4
+# PYTHON_INSTALLVERSION=3.9-slim-buster # for RASPI 4 funktioniert nicht mit lgpio
 
 dockerimage="tcpdevice2ppmpconnector"
 
@@ -24,11 +28,8 @@ echo -e " build dockerimage: $dockerimage  Build Version:" $Build
 
 
 
-
-
-
 # sudo docker build --build-arg NODE_ENV=development --build-arg BUILDCMD=dev -f Dockerfile.MH -t grafana/grafana-mhdev:v7.6.5 .
-sudo docker build -t ${dockerimage}:${Build}  -f Dockerfile .
+sudo docker build -t ${dockerimage}:${Build} --build-arg PYTHON_VERSION=${PYTHON_INSTALLVERSION} -f Dockerfile .
 
 # @docker build . -t %dockerimage%:%Build%
 
